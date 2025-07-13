8ace4b74f957e15f908478bd793fa3bad860c4ba
nilay-goyal <nilay800@gmail.com>
2025-07-13 00:54:45 +0000

    Install Mixpanel SDK
    Added Mixpanel SDK and initialization code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import mixpanel from 'mixpanel-browser'
+
+// Initialize Mixpanel
+mixpanel.init("YOUR_TOKEN", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
